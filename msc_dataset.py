99982b128b13965b618eb32846c9f1e8ba8998f9
Luciano Scarpino <lucianoscarp@gmail.com>
2025-10-23 21:42:50 +0200

    Initial Commit

---
+++ msc_dataset.py
@@ -0,0 +1,47 @@
+import torchaudio
+import torch
+import os
+
+class MSCDataset(object):
+    """
+    INPUT:
+    - data_path : str -> path containing audio files in .wav format
+    - class_labels : list[str] -> list containing ordered class labels
+
+    """
+    def __init__(self,data_path : str,class_labels : list[str]):
+        self.data = self.unpack_audios_(data_path)
+        self.encoded_labels = self.encode_labels_(class_labels)
+        self.labels = class_labels
+
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self,idx):
+        waveform, sampling_rate = self.data[idx]
+        return {
+            'x': torch.tensor(waveform,dtype=torch.float32),
+            'sampling_rate': sampling_rate,
+            'label': self.labels[idx]
+        }
+    
+    def unpack_audios_(self,path):
+        audios = []
+        for file_name in os.listdir(path):
+            if file_name.endswith(".wav"):
+                file_path = os.path.join(path,file_name)
+                waveform,sample_rate = torchaudio.load(file_path)
+                audios.append((waveform,sample_rate))
+
+        return audios
+    
+    def encode_labels_(self,labels):
+        encoded = {}
+        for idx,label in enumerate(labels):
+            encoded[label] = idx
+
+        return encoded
+    
+    def label_to_int(self,label):
+        if label in self.encoded_labels.keys():
+            return self.encoded_labels[label]
