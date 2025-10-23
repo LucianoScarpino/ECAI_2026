c547e0cd365a420515592bee232c922d7c718f46
Luciano Scarpino <lucianoscarp@gmail.com>
2025-10-23 22:38:10 +0200

    dataset

--- msc_dataset.py
+++ msc_dataset.py
@@ -1,37 +1,39 @@
-import torchaudio
+from torch.utils.data import Dataset
+
 import torch
+import torchaudio
 import os
 
-class MSCDataset(object):
+class MSCDataset(Dataset):
     """
     INPUT:
     - data_path : str -> path containing audio files in .wav format
-    - class_labels : list[str] -> list containing ordered class labels
+    - classes : list[str] -> list containing ordered class labels
 
     """
-    def __init__(self,data_path : str,class_labels : list[str]):
+    def __init__(self, data_path: str, classes: list[str]):
         self.data = self.unpack_audios_(data_path)
-        self.encoded_labels = self.encode_labels_(class_labels)
-        self.labels = class_labels
+        self.encoded_labels = self.encode_labels_(classes)
 
     def __len__(self):
         return len(self.data)
     
     def __getitem__(self,idx):
-        waveform, sampling_rate = self.data[idx]
+        waveform, sampling_rate, label = self.data[idx]
         return {
             'x': torch.tensor(waveform,dtype=torch.float32),
             'sampling_rate': sampling_rate,
-            'label': self.labels[idx]
+            'label': label
         }
     
     def unpack_audios_(self,path):
         audios = []
         for file_name in os.listdir(path):
             if file_name.endswith(".wav"):
+                label = file_name.split("_")[0]
                 file_path = os.path.join(path,file_name)
                 waveform,sample_rate = torchaudio.load(file_path)
-                audios.append((waveform,sample_rate))
+                audios.append((waveform,sample_rate,label))
 
         return audios
     
@@ -41,6 +43,9 @@ class MSCDataset(object):
             encoded[label] = idx
 
         return encoded
+    
+    def classes(self,label):
+        return self.encoded_labels[label]
     
     def label_to_int(self,label):
         if label in self.encoded_labels.keys():
